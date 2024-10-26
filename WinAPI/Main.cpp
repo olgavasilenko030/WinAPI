284bcc507dcb853e7443720324dbb6fd232260bc
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2024-10-27 00:17:33 +0500

    Add project files.

---
+++ WinAPI/Main.cpp
@@ -0,0 +1,19 @@
+#include<Windows.h>
+#define MESSAGE_BOX
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR IpCmdLine, INT nCmdShow) {
+	MessageBox
+	(
+#ifdef MESSAGE_BOX
+
+		NULL,
+		"Hello WinAPI!\nЭто MessageBox,деткаю",
+		"Заголовок окна",
+		MB_YESNO | MB_ICONQUESTION
+		| MB_HELP
+		| MB_DEFBUTTON4
+		| MB_SYSTEMMODAL
+	);
+	return 0;
+#endif // 
+
+}
