6d17c1f8c6dacfe2b5780555cdb6af1f965a1560
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2024-11-05 23:43:19 +0500

    AddItem to ListBox DONE

--- ListBox2/main.cpp
+++ ListBox2/main.cpp
@@ -6,6 +6,7 @@
 CONST CHAR* g_VALUES[] = { "This", "is","my","first","List","Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hIntance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -19,7 +20,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 	{
-		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE (IDI_ICON1));
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		// GetModuleHendle(NULL); возвращает hInstance нашей программы
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
@@ -32,20 +33,66 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_ADD:
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, (DLGPROC)DlgProcAddItem, 0);
+			break;
 		case IDOK:
 		{
 			CONST INT SIZE = 256;
 			CHAR sz_buffer[SIZE]{};
 
 			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
-			INT i = SendNotifyMessage(hList, LB_GETCURSEL, 0, 0);
+			INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
 			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
 
 			CHAR sz_message[SIZE]{};
 			sprintf(sz_message, "Вы выбрали пункт №%i со значением \"%s\".", i, sz_buffer);
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
 		}
+		break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
 			break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
+BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD));
+
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			HWND hEditAdd = GetDlgItem(hwnd, IDC_EDIT_ADD);
+			SendMessage(hEditAdd, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+
+			HWND hList = GetDlgItem(GetParent(hwnd), IDC_LIST1);
+			//GetParent (); возвращает родительское окно (HWND родительского окна ) для указанного окна
+			if (SendMessage(hList, LB_FINDSTRING, -1, (LPARAM)sz_buffer) == LB_ERR)
+			{
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			}
+			else
+			{
+				INT result = MessageBox(hwnd, "Такое вхождение уже есть в списке, введите другое значение", "Info", MB_YESNO | MB_ICONQUESTION);
+				if (result == IDYES)break;
+			}
+
+		}
+
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 			break;
