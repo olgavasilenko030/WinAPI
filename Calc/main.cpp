f1ae2a94c1187c2847cbe1237a63a8a5b928b005
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2024-11-28 22:48:57 +0500

    Point bug fixed

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -307,7 +307,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				//ZeroMemory(sz_display,SIZE);
 				sz_display[0] = 0;
 			}
-			if (!input && input_operation)sz_display[0] = 0;
+			if (!input && input_operation /* && !strchr(sz_display, '.')*/)sz_display[0] = 0;
 
 			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			if (strlen(sz_display) == 1 && sz_display[0] == '0')
@@ -320,9 +320,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 		if (LOWORD(wParam) == IDC_BUTTON_POINT)
 		{
-			if (strchr(sz_display, '.')) break;
-			strcat(sz_display, ".");
+			if (strchr(sz_display, '.')&& input) break;
+			if (input_operation && a == atof(sz_display))strcpy(sz_display, "0.");
+			else strcat(sz_display, ".");
+			//strcat(sz_display, ".");
 			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+			input = TRUE;
 		}
 
 		//if (LOWORD(wParam) == IDC_EDIT_DISPLAY && HIWORD(wParam) == EN_SETFOCUS)		SetFocus(hwnd);
